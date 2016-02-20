2b8f2e50f6cbea2d5f0614224598019baa9cc87b
Paul Ganssle <paul@ganssle.io>
2016-02-20 12:38:16 -0500

    Renamed zonefile_metadata.

--- update_index.sh
+++ update_index.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 git checkout master config.yml config.py fetch_tz.py generate_index.py \
                     generate_metadata.py generate_signatures.py \
                     base_metadata.json tzdata_files.py
