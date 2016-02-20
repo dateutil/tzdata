3f2851cfda9f100c962e43541e52b486c71f71c9
Paul Ganssle <paul@ganssle.io>
2016-02-20 12:28:17 -0500

    Add reset to update_index.sh

--- update_index.sh
+++ update_index.sh
@@ -4,6 +4,8 @@ git checkout master config.yml config.py fetch_tz.py generate_index.py \
                     generate_metadata.py generate_signatures.py \
                     base_metadata.json tzdata_files.py
 
+git reset
+
 python fetch_tz.py
 python generate_signatures.py
 python generate_metadata.py
