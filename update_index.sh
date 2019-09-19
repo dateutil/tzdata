ed87297ac6a5e24de35a589b93ea3306e7615693
Paul Ganssle <paul@ganssle.io>
2019-09-19 11:42:34 +0100

    Improve update_index.sh script
    This creates a virtual environment (if it doesn't already exist) and
    brings in the latest requirements when running the updater. Previous
    versions of this script relied on the user already having installed
    unspecified dependencies.

--- update_index.sh
+++ update_index.sh
@@ -2,11 +2,26 @@
 set -e
 git checkout master config.yml config.py fetch_tz.py generate_index.py \
                     generate_metadata.py generate_signatures.py \
-                    base_metadata.json tzdata_files.py
+                    base_metadata.json tzdata_files.py requirements.txt
 
 git reset
 
-python fetch_tz.py
-python generate_signatures.py
-python generate_metadata.py
-python generate_index.py
+# Activate or create a virtualenv
+VENV_DIR=".venv"
+ACTIVATE_LOC="${VENV_DIR}/bin/activate"
+
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv ${VENV_DIR}
+fi
+
+source ${ACTIVATE_LOC}
+
+# Update all the requirements
+pip install -U pip
+pip install -U -r requirements.txt
+
+# Run the relevant scripts
+python3 fetch_tz.py
+python3 generate_signatures.py
+python3 generate_metadata.py
+python3 generate_index.py
